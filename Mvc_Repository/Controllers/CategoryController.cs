557c0b8d0ead5e99d63877066ad73372fb8ba00d
Yida Chen <yida820310@gmail.com>
2018-05-26 13:00:12 +0800

    Part.1 初學者的起手式

---
+++ Mvc_Repository/Controllers/CategoryController.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using Mvc_Repository.Models;
+using Mvc_Repository.Models.Interface;
+using Mvc_Repository.Models.Repository;
+
+namespace Mvc_Repository.Controllers
+{
+    public class CategoryController : Controller
+    {
+        private ICategoryRepository categoryRepository;
+
+        public CategoryController()
+        {
+            this.categoryRepository = new CategoryRepository();
+        }
+
+
+        // GET: Category
+        public ActionResult Index()
+        {
+            var categories = this.categoryRepository.GetAll().ToList();
+            return View(categories);
+        }
+
+        // GET: Category/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            var category = this.categoryRepository.Get(id.Value);
+            if (category == null)
+            {
+                return HttpNotFound();
+            }
+            return View(category);
+        }
+
+        // GET: Category/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Category/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "CategoryID,CategoryName,Description,Picture")] Categories category)
+        {
+            if (ModelState.IsValid)
+            {
+                this.categoryRepository.Create(category);
+                return RedirectToAction("Index");
+            }
+
+            return View(category);
+        }
+
+        // GET: Category/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            var category = this.categoryRepository.Get(id.Value);
+            if (category == null)
+            {
+                return HttpNotFound();
+            }
+            return View(category);
+        }
+
+        // POST: Category/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "CategoryID,CategoryName,Description,Picture")] Categories category)
+        {
+            if (ModelState.IsValid)
+            {
+                this.categoryRepository.Update(category);
+                return RedirectToAction("Index");
+            }
+            return View(category);
+        }
+
+        // GET: Category/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            var category = this.categoryRepository.Get(id.Value);
+            if (category == null)
+            {
+                return HttpNotFound();
+            }
+            return View(category);
+        }
+
+        // POST: Category/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            var category = this.categoryRepository.Get(id);
+            this.categoryRepository.Delete(category);
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ Mvc_Repository/Controllers/ProductController.cs
@@ -0,0 +1,135 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using Mvc_Repository.Models;
+using Mvc_Repository.Models.Interface;
+using Mvc_Repository.Models.Repository;
+
+namespace Mvc_Repository.Controllers
+{
+    public class ProductController : Controller
+    {
+        private IProductRepository productRepository;
+        private ICategoryRepository categoryRepository;
+
+        public ProductController()
+        {
+            this.productRepository = new ProductRepository();
+            this.categoryRepository = new CategoryRepository();
+        }
+        private IEnumerable<Categories> Categories
+        {
+            get
+            {
+                return categoryRepository.GetAll();
+            }
+        }
+        // GET: Product
+        public ActionResult Index()
+        {
+            var products = productRepository.GetAll().ToList();
+            return View(products);
+        }
+
+        // GET: Product/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Products product = productRepository.Get(id.Value);
+            if (product == null)
+            {
+                return HttpNotFound();
+            }
+            return View(product);
+        }
+
+        // GET: Product/Create
+        public ActionResult Create()
+        {
+            ViewBag.CategoryID = new SelectList(this.Categories, "CategoryID", "CategoryName");
+            return View();
+        }
+
+        // POST: Product/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "ProductID,ProductName,SupplierID,CategoryID,QuantityPerUnit,UnitPrice,UnitsInStock,UnitsOnOrder,ReorderLevel,Discontinued")] Products products)
+        {
+            if (ModelState.IsValid)
+            {
+                this.productRepository.Create(products);
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.CategoryID = new SelectList(this.Categories, "CategoryID", "CategoryName", products.CategoryID);
+            return View(products);
+        }
+
+        // GET: Product/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Products product = this.productRepository.Get(id.Value);
+            if (product == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.CategoryID = new SelectList(this.Categories, "CategoryID", "CategoryName", product.CategoryID);
+            return View(product);
+        }
+
+        // POST: Product/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "ProductID,ProductName,SupplierID,CategoryID,QuantityPerUnit,UnitPrice,UnitsInStock,UnitsOnOrder,ReorderLevel,Discontinued")] Products products)
+        {
+            if (ModelState.IsValid)
+            {
+                this.productRepository.Update(products);
+                return RedirectToAction("Index");
+            }
+            ViewBag.CategoryID = new SelectList(this.Categories, "CategoryID", "CategoryName", products.CategoryID);
+            return View(products);
+        }
+
+        // GET: Product/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Products product = this.productRepository.Get(id.Value);
+            if (product == null)
+            {
+                return HttpNotFound();
+            }
+            return View(product);
+        }
+
+        // POST: Product/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Products product = this.productRepository.Get(id);
+            this.productRepository.Delete(product);
+            return RedirectToAction("Index");
+        }
+    }
+}
