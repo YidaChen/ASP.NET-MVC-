d2c7ebbf15318ea42b8ecc58aeae0b60b9f3231f
Yida Chen <yida820310@gmail.com>
2018-05-27 19:41:33 +0800

    Part.4 - 抽出 Model 層並建立為類別庫專案

--- Mvc_Repository/App_Start/RouteConfig.cs
+++ Mvc_Repository/App_Start/RouteConfig.cs
@@ -5,7 +5,7 @@ using System.Web;
 using System.Web.Mvc;
 using System.Web.Routing;
 
-namespace Mvc_Repository
+namespace Mvc_Repository.Web
 {
     public class RouteConfig
     {
@@ -16,7 +16,7 @@ namespace Mvc_Repository
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "Product", action = "Index", id = UrlParameter.Optional }
             );
         }
     }
